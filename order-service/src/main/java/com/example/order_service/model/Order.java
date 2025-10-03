e2d9b98163518968f905f8a8d687f22c0d06184b
Yogesh <117355987+1Yogeshh@users.noreply.github.com>
2025-10-03 11:54:17 +0530

    Update Order.java

--- order-service/src/main/java/com/example/order_service/model/Order.java
+++ order-service/src/main/java/com/example/order_service/model/Order.java
@@ -18,7 +18,6 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    // private String product;
     private Long userId;
     private Long productId;
 }
